becbd02c11ad02222ac27dd7edca498443965a61
max26 <blizzard005@gmail.com>
2014-10-29 21:35:26 +0300

    Update ChampernowneConstant-find-any-digit.c

--- ChampernowneConstant-find-any-digit.c
+++ ChampernowneConstant-find-any-digit.c
@@ -1,71 +1,91 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
 int findChampernowneDigit(int m)
 {	
-	//  m  -  is  m-th digit of Champernowne constant that has to be found
-	long N[10];// an array  for keeping  of all 1-digit, all 2-digit,all 3-digit numbers 
-	char Dbuffer[10]; /* char array for storing last k-digit number (number x)
-	   which contains m-th digit of our sequence on y-th place from beginning of number x */
-	long x1=0,x2=0,y;
+   //  m  - это m-ая цифра константы Чамперноуна, которую нужно найти
+    long N[10];// массив для хранения всех 1-значных чисел,2-хзначных чисел,3-хзначных чисел,....
+    char Dbuffer[10];/* массив для хранения последнего k-значного числа (число x2),которое содержит m-ое число нашей последовательности на y-ом месте,считая от начала числа x2 */
+    long x1=0,x2=0,y;
+    int k,i,result;
+    int remainder2; 
 
-	int k,i,result;
+    for (i=0;i<10;i++)
+    {
+        N[i]=0;
+    }       
+    if  (m==1)
+    {
+        x2=1;
+        y=1;         
+    }
+    else
+    {          
+        for (k=1;N[k-1]<=m;k++)
+        {    
+            N[k] = N[k-1] + (k)*9*((int)(pow((double)10,k-1)));           
+        }
+    
+        x1 = (m - N[k-2])/(k-1) ;
 
-	for (i=0;i<10;i++)
-	{
-		N[i]=0;
-	}
+        if (x1<1)
+        {
+            x1=1;
+            x2 = x1 + (int)pow((double)10,1)+1;
+            y=1;
+		printf("x1 = %d\n\n",x1);
+        }               
+        else
+        {
+            x2 = x1 + (int)pow((double)10,k-2)-1;  //  target (k-1)-th number that we're lookin for
+            y = (m - N[k-2]) - x1*(k-1);
 
-	k=1;	
-
-	if  (m==1)
-	{
-		x2=1;
-		y=1;          
-	}
-	else
-	{			
-		for (k=1;N[k-1]<=m;k++)
-		{ 
-		    N[k] = N[k-1] + (k)*9*((int)(pow((double)10,k-1)));
-		    //	printf("N[%d] = %d\n\n",k,N[k]);
-		}
-
-		x1 = (int) floor( (double)((m - N[k-2])/(k-1)) );
-		if (x1<1)
-		{
-			x1=1;
-			x2 = x1 + (int)pow((double)10,1)-1; 
-			y=1;// printf("x1 = %d\n\n",x1);
-		}	             
-		else
-		{ 
-			x2 = x1 + (int)pow((double)10,k-2)-1;  //  target (k-1)-th number that we're lookin for
-			y = (m - N[k-2]) - x1*(k-1);
-		}	
-	}
-	itoa(x2,Dbuffer,10);
-//	printf("Dbuffer=%s\n",Dbuffer);
-//	printf("Dbuffer[%d]=%c\n",y-1,Dbuffer[y-1]);
-	result = Dbuffer[y-1]-'0';
-//	printf("result=%d\n",result);
-	return result;	
+			remainder2 = (m - N[k-2])%(x1*(k-1));
+			if (remainder2 == 0) { y = k-1;}
+        }  
+    }
+    itoa(x2,Dbuffer,10);
+    result = Dbuffer[y-1]-'0';
+    return result;    
 }
 
+
 void  main()
-{
-	int i,num[]={1,10,100,1000,10000,100000,1000000};
+{ 
+	//clock_t t;
+ // int f;
+ // t = clock();	
+	/*struct timeval  tv1, tv2;
+gettimeofday(&tv1, NULL);*/
+	clock_t begin, end;
+    double time_spent;
+
+begin = clock();
+/* here, do your time-consuming job */
+    int i;
+	int num[]={1,10,100,1000,10000,100000,1000000};
 	int product=1,digit[10];
 
 	for (i=0;i<(sizeof(num)/sizeof(num[0]));i++)
 	{ 		
 		digit[i]=findChampernowneDigit(num[i]);
-		printf("the %d-th digit of sequence is %d\n\n",num[i],digit[i]);
+		//printf("the %d-th digit of sequence is %d\n\n",num[i],digit[i]);
 		product *= digit[i] ;
 	}
-	printf("digit on %d-th place is %d\n",28383,findChampernowneDigit(28383));	
-	printf("digit on %d-th place is %d\n",206788,findChampernowneDigit(206788));	
+	int dig=1001;
+	printf("digit on %i-th place is %i\n",dig,findChampernowneDigit(dig));	
+	//printf("digit on %d-th place is %d\n",28383,findDigit(28383));	
+	//printf("digit on %d-th place is %d\n",206788,findDigit(206788));	
 	printf("Product of d1*d10*d100*d1000*d10000*d100000*d1000000 = %d\n\n",product);
-	getchar();
+
+	end = clock();
+   time_spent = (double)(end - begin); /* CLOCKS_PER_SEC;*/
+  //printf("\nTime of execution is %f\n",time_spent);
+	//t = clock() - t;
+  //printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+   printf ("It took me %f clicks (%f seconds).\n",time_spent,(time_spent)/CLOCKS_PER_SEC);
+
+   getchar();
 }
