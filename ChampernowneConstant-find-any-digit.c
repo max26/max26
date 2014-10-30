ae4137abaec5e9bec77841cf8a606df4848e88c5
max26 <blizzard005@gmail.com>
2014-10-30 11:10:11 +0300

    Update ChampernowneConstant-find-any-digit.c

--- ChampernowneConstant-find-any-digit.c
+++ ChampernowneConstant-find-any-digit.c
@@ -18,9 +18,9 @@ int findChampernowneDigit(int m)
     (this is number x2 , that contains our m-th digit of sequence on y-th place,if count from beginning of number x2
     As arrays in C are zero-based , so our target digit is on (y-1)-th place in number x2 
     */
-    long x1=0,x2=0,y;
+      long x1=0,x2=0,y=0;
     int k,i,result;
-    int remainder2; 
+    int remainder; 
 
     for (i=0;i<10;i++)
     {
@@ -36,24 +36,34 @@ int findChampernowneDigit(int m)
         for (k=1;N[k-1]<=m;k++)
         {    
             N[k] = N[k-1] + (k)*9*((int)(pow((double)10,k-1)));           
-        }
-    
+        }    
         x1 = (m - N[k-2])/(k-1) ;
-
         if (x1<1)
-        {
-            x1=1;
-            x2 = x1 + (int)pow((double)10,1)+1;
+        {   
+			if ((m - N[k-2])%(k-1) == 0)
+			{
+			   x1=0;
+			}
+			else
+			{
+		          x1=1;
+			}
+            x2 = x1 + (int)pow((double)10,1)-1;
             y=1;
-		printf("x1 = %d\n\n",x1);
         }               
         else
-        {
-            x2 = x1 + (int)pow((double)10,k-2)-1;  //  target (k-1)-th number that we're lookin for
-            y = (m - N[k-2]) - x1*(k-1);
-
-			remainder2 = (m - N[k-2])%(x1*(k-1));
-			if (remainder2 == 0) { y = k-1;}
+        {	
+			remainder = (m - N[k-2])%(k-1);
+			if (remainder == 0) 
+			{
+				x2 = x1 + (int)pow((double)10,k-2)-1;  //  target (k-1)-th number that we're lookin for
+				y = (m - N[k-2]) - x1*(k-1)+(k-1);	
+			}
+			else
+			{
+				x2 = x1 + (int)pow((double)10,k-2);  //  target (k-1)-th number that we're lookin for
+				y = (m - N[k-2]) - x1*(k-1);
+			}		
         }  
     }
     itoa(x2,Dbuffer,10);
@@ -75,7 +85,7 @@ gettimeofday(&tv1, NULL);*/
 begin = clock();
 /* here, do your time-consuming job */
     int i;
-	int num[]={1,10,100,1000,10000,100000,1000000};
+	int num[]={1,10,100,1000,10000,100000,1000000,100000000};
 	int product=1,digit[10];
 
 	for (i=0;i<(sizeof(num)/sizeof(num[0]));i++)
@@ -84,12 +94,11 @@ begin = clock();
 		//printf("the %d-th digit of sequence is %d\n\n",num[i],digit[i]);
 		product *= digit[i] ;
 	}
-	int dig=1001;
+	int dig=1005;
 	printf("digit on %i-th place is %i\n",dig,findChampernowneDigit(dig));	
-	//printf("digit on %d-th place is %d\n",28383,findDigit(28383));	
-	//printf("digit on %d-th place is %d\n",206788,findDigit(206788));	
-	printf("Product of d1*d10*d100*d1000*d10000*d100000*d1000000 = %d\n\n",product);
-
+	printf("digit on %d-th place is %d\n",28383,findChampernowneDigit(28383));	
+	printf("digit on %d-th place is %d\n",206788,findChampernowneDigit(206788));	
+	printf("Product of d1*d10*d100*d1000*d10000*d100000*d1000000*d100000000 = %d\n\n",product);
 	end = clock();
    time_spent = (double)(end - begin); /* CLOCKS_PER_SEC;*/
   //printf("\nTime of execution is %f\n",time_spent);
