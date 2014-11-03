657927927ebf6c6d370abe69de99bba5ee85b967
max26 <blizzard005@gmail.com>
2014-11-03 15:38:31 +0300

    Update ChampernowneConstant-find-any-digit.c

--- ChampernowneConstant-find-any-digit.c
+++ ChampernowneConstant-find-any-digit.c
@@ -51,30 +51,30 @@ int findChampernowneDigit(unsigned long long m)
 
         if (x1<1)
         {   
-			if (remainder == 0)
-			{
-			  x1=0;
-			}
-			else
-			{
-		      x1=1;
-			}
+	    if (remainder == 0)
+	    {
+	        x1=0;
+	    }
+	    else
+	    {
+		x1=1;
+	    }
             x2 = x1 + (ull)pow((double)10,1)-1;
             y=1;
         }               
         else
         {	
-			if (remainder == 0) 
-			{
-				x2 = x1 + (ull)pow((double)10,k-2)-1;  //  target (k-1)-th number that we're lookin for
-				//y = (m - N[k-2]) - x1*(k-1)+(k-1);	
-				y = k-1;
-			}
-			else
-			{
-				x2 = x1 + (ull)pow((double)10,k-2);  //  target (k-1)-th number that we're lookin for
-				y = (m - N[k-2]) - x1*(k-1);
-			}		
+		if (remainder == 0) 
+		{
+			x2 = x1 + (ull)pow((double)10,k-2)-1;  //  target (k-1)-th number that we're lookin for
+			//y = (m - N[k-2]) - x1*(k-1)+(k-1);	
+			y = k-1;
+		}
+		else
+		{
+			x2 = x1 + (ull)pow((double)10,k-2);  //  target (k-1)-th number that we're lookin for
+			y = (m - N[k-2]) - x1*(k-1);
+		}		
         }  
     }
     ulltoa(x2,Dbuffer,10);
@@ -89,35 +89,35 @@ void  main()
  // t = clock();	
 	/*struct timeval  tv1, tv2;
 gettimeofday(&tv1, NULL);*/
-	clock_t begin, end;
-    double time_spent;
+clock_t begin, end;
+double time_spent;
 
 begin = clock();
 /* here, do your time-consuming job */
-    int i;
-	int num[]={1,10,100,1000,10000,100000,1000000};
-	int product=1,digit[10];
+int i;
+int num[]={1,10,100,1000,10000,100000,1000000};
+int product=1,digit[10];
 
-	for (i=0;i<(sizeof(num)/sizeof(num[0]));i++)
-	{ 		
-		digit[i]=findChampernowneDigit(num[i]);
-		//printf("the %d-th digit of sequence is %d\n\n",num[i],digit[i]);
-		product *= digit[i] ;
-	}
+for (i=0;i<(sizeof(num)/sizeof(num[0]));i++)
+{ 		
+        digit[i]=findChampernowneDigit(num[i]);
+	//printf("the %d-th digit of sequence is %d\n\n",num[i],digit[i]);
+	product *= digit[i] ;
+}
 
-	int dig1=1001;
-	ull dig2=100000000005;
-	printf("digit on %d-th place is %d\n",dig1,findChampernowneDigit(dig1));
-	printf("digit on %llu-th place is %d\n",dig2,findChampernowneDigit(dig2));
-	printf("digit on %d-th place is %d\n",28383,findChampernowneDigit(28383));	
-	printf("digit on %d-th place is %d\n",206788,findChampernowneDigit(206788));	
-	printf("Product of d1*d10*d100*d1000*d10000*d100000*d1000000 = %d\n\n",product);
-	end = clock();
-   time_spent = (double)(end - begin); /* CLOCKS_PER_SEC;*/
-  //printf("\nTime of execution is %f\n",time_spent);
-	//t = clock() - t;
-  //printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
-   printf ("It took me %f clicks (%f.10 seconds).\n",time_spent,(time_spent)/CLOCKS_PER_SEC);
+int dig1=1001;
+ull dig2=100000000005;
+printf("digit on %d-th place is %d\n",dig1,findChampernowneDigit(dig1));
+printf("digit on %llu-th place is %d\n",dig2,findChampernowneDigit(dig2));
+printf("digit on %d-th place is %d\n",28383,findChampernowneDigit(28383));	
+printf("digit on %d-th place is %d\n",206788,findChampernowneDigit(206788));	
+printf("Product of d1*d10*d100*d1000*d10000*d100000*d1000000 = %d\n\n",product);
+end = clock();
+time_spent = (double)(end - begin); /* CLOCKS_PER_SEC;*/
+ //printf("\nTime of execution is %f\n",time_spent);
+//t = clock() - t;
+ //printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+printf ("It took me %f clicks (%f.10 seconds).\n",time_spent,(time_spent)/CLOCKS_PER_SEC);
 
-   getchar();
+getchar();
 }
